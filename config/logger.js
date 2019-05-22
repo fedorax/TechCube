cfa183ef57013b7f42eb27dffc3609039178ba23
fedorax <fedorax@hotmail.co.jp>
2019-05-22 22:44:25 +0900

    Add log4js

---
+++ config/logger.js
@@ -0,0 +1,7 @@
+'use strict';
+const path = require('path');
+const log4js = require('log4js');
+const log4js_config = require(path.resolve('config/log4js.json'));
+log4js.configure(log4js_config);
+
+module.exports = log4js;
